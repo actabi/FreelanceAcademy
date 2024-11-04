1591db6e39bbed419326c3e8179d249689c0d321
Guillaume De Laroque <gdelaroque@erama.co>
2024-11-04 14:05:42 +0100

    Maj

--- src/config/main.ts
+++ src/config/main.ts
@@ -1,3 +1,6 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "../../app.module";
+
 // src/main.ts
 const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'log', 'debug']
