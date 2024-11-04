3a81ab6b8ab663bd1bbe7f4290137fe15c85fce4
Guillaume De Laroque <gdelaroque@erama.co>
2024-11-04 22:43:18 +0100

    MaJ 04

--- data-source.ts
+++ data-source.ts
@@ -1,19 +1,17 @@
 import { DataSource } from 'typeorm';
 import { ConfigService } from '@nestjs/config';
 import * as dotenv from 'dotenv';
+import { MissionEntity } from './src/infrastructure/database/entities/mission.entity';
 
-// Charger les variables d'environnement
 dotenv.config();
-
-const configService = new ConfigService();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  entities: ['src/**/*.entity{.ts,.js}'],
-  migrations: ['src/infrastructure/database/migrations/*{.ts,.js}'],
+  entities: [MissionEntity],
+  migrations: ['src/infrastructure/database/migrations/*.ts'],
   ssl: {
-    rejectUnauthorized: false // Nécessaire pour Railway
+    rejectUnauthorized: false
   },
   synchronize: false,
   logging: true
