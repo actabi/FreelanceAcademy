d70025f95e2914260a68030106cdf8e2b27f64be
Guillaume De Laroque <gdelaroque@erama.co>
2024-11-09 19:32:30 +0100

    d

--- src/core/infrastructure/database/migrations/data-source.ts
+++ src/core/infrastructure/database/migrations/data-source.ts
@@ -1,23 +1,14 @@
-// data-source.ts
+// src/core/infrastructure/database/migrations\data-source.ts
 import { DataSource, DataSourceOptions } from 'typeorm';
 import * as dotenv from 'dotenv';
-import { MissionEntity } from './src/core/domain/entities/mission.entity';
-import { SkillEntity } from './src/core/domain/entities/skill.entity';
+import { MissionEntity } from '../../../domain/entities/mission.entity';
+import { SkillEntity } from '../../../domain/entities/skill.entity';
 
 dotenv.config();
 
-const getDatabaseUrl = () => {
-  // En développement, utilisez l'URL de la base de données locale
-  if (process.env.NODE_ENV === 'development') {
-    return process.env.DATABASE_URL_LOCAL;
-  }
-  // En production, utilisez l'URL Railway
-  return process.env.DATABASE_URL;
-};
-
 const options: DataSourceOptions = {
     type: 'postgres',
-    url: getDatabaseUrl(),
+    url: process.env.DATABASE_URL_LOCAL + "?family=0",
     entities: [MissionEntity, SkillEntity ],
     migrations: ['src/core/infrastructure/database/migrations/*.ts'],
     ssl: process.env.NODE_ENV === 'production' ? {
